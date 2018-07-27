75b76f262456387a87ac98842749a3ed72002832
Roman Macek <roman.macek@valatron.com>
2018-07-27 11:44:04 +0200

    testovani pomoci Mock

---
+++ NUnitApp2/ZdrojDatProNunit/IWebovaSluzba.cs
@@ -0,0 +1,7 @@
+﻿namespace ZdrojDatProNunit
+{
+    public interface IWebovaSluzba
+    {
+        bool Autentification(string username, string password);
+    }
+}
---
+++ NUnitApp2/ZdrojDatProNunit/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ZdrojDatProNunit
+{
+    public struct User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public bool Pokus()
+        {
+            return true;
+        }
+    }
+}
