a6e5713a9610b2ed2b5e23422cf4ebd3d5f2f0b5
Roman Macek <roman.macek@valatron.com>
2018-08-21 12:45:28 +0200

    OData aplikace

---
+++ ODataSolution/BookStore/Models/BookStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models
+{
+    public class BookStoreContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Press> Press { get; set; }
+
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().OwnsOne(c => c.Location);
+        }
+    }
+}
---
+++ ODataSolution/BookStore/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OData.Edm;
+
+namespace BookStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;         
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BookStoreContext>(opt => opt.UseInMemoryDatabase("BookLists")); // Za druhé, měli bychom registrovat databázový kontext pomocí 
+                                                        // vestavěné injekce závislostí v konfiguraci služby.Takže změňte metodu "ConfigureServices" v třídě "Startup" 
+            services.AddOData();  // ASP.NET Core OData vyžaduje některé služby registrované vpřed, aby poskytly své funkce. 
+                                  // Knihovna poskytuje metodu rozšíření nazvanou "AddOData ()" pro registraci požadovaných služeb OData prostřednictvím vestavěné závislostí. 
+                                  // Do třídy "Spuštění" vložte následující metody do metody "Konfigurace služeb":
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseMvc();
+
+            // Také je třeba přidat cestu OData k registraci koncového bodu OData.
+            // Přidáme cestu OData s názvem "odata" s předponou "odata" na trasách MVC a zavoláme "GetEdmModel()", 
+            // abychom model Edm spojili s koncovým bodem.Takže změňte metodu "Konfigurovat ()" v třídě "Startup" jako:
+            app.UseMvc(b =>
+            {
+                b.Select().Expand().Filter().OrderBy().MaxTop(100).Count();  //Přidání následujícího řádku kódu do Startup.cs umožňuje všechny možnosti dotazu OData, například $ filter, $ orderby, $ expand, atd.
+                b.MapODataServiceRoute("odata", "odata", GetEdmModel());
+            });
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Book>("Books");
+            builder.EntitySet<Press>("Presses");
+            return builder.GetEdmModel();
+        }
+    }
+}
