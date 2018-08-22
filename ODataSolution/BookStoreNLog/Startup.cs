b3667e74ca0b5370037fa27dcfcdcd27bbab12f9
Roman Macek <roman.macek@valatron.com>
2018-08-22 13:03:42 +0200

    NLog zapis do vice souboru

--- ODataSolution/BookStoreNLog/Startup.cs
+++ ODataSolution/BookStoreNLog/Startup.cs
@@ -40,12 +40,12 @@ namespace BookStore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) //, ILoggerFactory loggerFactory
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-            loggerFactory.AddNLog();
-            env.ConfigureNLog("nlog.config");
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+            //loggerFactory.AddNLog();
+            //env.ConfigureNLog("nlog.config");
 
             if (env.IsDevelopment())
             {
