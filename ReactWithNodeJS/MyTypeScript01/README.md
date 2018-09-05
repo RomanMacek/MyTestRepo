7f76eb0229077e17194770a0f73a0a1cd81402bb
Roman Macek <roman.macek@valatron.com>
2018-09-05 13:33:34 +0200

    nový TypeScript projekt

---
+++ ReactWithNodeJS/MyTypeScript01/README.md
@@ -0,0 +1,3 @@
+﻿# MyTypeScript01
+
+
---
+++ ReactWithNodeJS/WebAppWithTypeScript/Pages/IndexOriginal.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppWithTypeScript.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
