b3e9b6c506dab2343584ac2506463ed4322e1b73
Roman Macek <roman.macek@valatron.com>
2018-09-10 08:58:42 +0200

    pokus o react komponentu

---
+++ ReactWithNodeJSMyTest/NodejsWebApp01/app.js
@@ -0,0 +1,30 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var require;
+var b = "nazdar";
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Hello = /** @class */ (function (_super) {
+    __extends(Hello, _super);
+    function Hello() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Hello.prototype.render = function () {
+        return (" <h1>Welcome to React!!</h1>");
+    };
+    return Hello;
+}(React.Component));
+//ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(Hello, document.getElementById('root'));
+//# sourceMappingURL=app.js.map
---
+++ ReactWithNodeJSMyTest/NodejsWebApp01/app.tsx
@@ -0,0 +1,16 @@
+﻿var require: any ;
+var b: string = "nazdar";
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class Hello extends React.Component {
+    render() {
+        return (
+           " <h1>Welcome to React!!</h1>"
+        );
+    }
+}
+
+//ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(Hello, document.getElementById('root'));
