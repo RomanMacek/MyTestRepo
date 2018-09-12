8f9a5bb9f9d81ccc777f9821b09baf710066596d
Roman Macek <roman.macek@valatron.com>
2018-09-12 11:39:00 +0200

    SinglePageApplication - test

--- SPAProjekt/OsobySPA/App_Start/BundleConfig.cs
+++ SPAProjekt/OsobySPA/App_Start/BundleConfig.cs
@@ -27,6 +27,7 @@ namespace OsobySPA
                 "~/Scripts/app/app.datamodel.js",
                 "~/Scripts/app/app.viewmodel.js",
                 "~/Scripts/app/home.viewmodel.js",
+                "~/Scripts/app/osoby.viewmodel.js", // Přidaný řádek
                 "~/Scripts/app/_run.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
---
+++ SPAProjekt/OsobySPA/Scripts/app/osoby.viewmodel.js
@@ -0,0 +1,14 @@
+﻿function OsobyViewModel(app, dataModel) {
+
+}
+
+// Registrace ViewModelu
+app.addViewModel({
+    name: "Osoby",
+    bindingMemberName: "osoby",
+    factory: OsobyViewModel
+});
+
+//Jako první si vytvoříme "třídu" OsobyViewModel.Tu následně registrujeme jako ViewModel SPA aplikace pomocí objektu app, 
+//který nám poskytuje a předgenerovalo ASP.NET.
+//Důležité je zejména bindingMemberName, pomocí kterého budeme z šablon k ViewModelu přistupovat.
